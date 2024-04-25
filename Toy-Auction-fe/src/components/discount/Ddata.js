929c032143ebcfcc94442676df5d1d6f9d1d33bd
Trần Văn Tài <taiho58@gmail.com>
2024-04-25 22:33:25 +0700

    fix(mock-data): change to new-name

--- Toy-Auction-fe/src/components/discount/Ddata.js
+++ Toy-Auction-fe/src/components/discount/Ddata.js
@@ -1,273 +1,48 @@
 const Ddata = [
   {
-    id: "TSTPNT2024040905",
+    cover: "./images/discount/discount-1.png",
     name: "BenuX 2022",
-    price: 250,
-    description:
-      "BenuX 2022 là một chiếc xe đạp đua cao cấp được thiết kế cho hiệu suất và tốc độ. Nó được trang bị khung xe carbon nhẹ và cứng cáp, hệ thống truyền động Shimano Ultegra 11 tốc độ mượt mà và chính xác, và phanh đĩa thủy lực mạnh mẽ. BenuX 2022 hoàn hảo cho những tay đua muốn nâng cao thành tích của họ hoặc đơn giản là muốn tận hưởng cảm giác lái xe đạp thể thao đỉnh cao.",
-    saleAndApplicationTime: {
-      from: "2024-04-09T00:00:00.000Z",
-      to: "2024-04-17T00:00:00.000Z",
-    },
-    depositTime: {
-      from: "2024-04-16T00:00:00.000Z",
-      to: "2024-04-19T00:00:00.000Z",
-    },
-    propertyInfo: {
-      startingPrice: 500000000,
-      applicationFee: 200000,
-      bidIncrement: 20000000,
-      deposit: 25000000,
-    },
-    auctionDetails: {
-      type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
-      method: "Phương thức trả giá lên",
-    },
-    auctionStartTime: "2024-04-20T09:00:00.000Z",
-    cover: [
-      require("./images/discount/discount-1.png"),
-      require("./images/discount/discount-1.png"),
-    ],
+    price: "$250",
   },
   {
-    id: "TSTPNT2024040905",
+    cover: "./images/discount/discount-2.png",
     name: "Sony TV 1080p",
-    price: 450,
-    description:
-      "Mang đến cho bạn trải nghiệm hình ảnh sắc nét và sống động với Sony TV 1080p. Chiếc TV này được trang bị màn hình 1080p Full HD, mang đến hình ảnh chi tiết và rõ ràng hơn so với TV độ phân giải tiêu chuẩn. Công nghệ tiên tiến của Sony giúp nâng cao chất lượng hình ảnh, cho phép bạn thưởng thức các chương trình truyền hình, phim ảnh và trò chơi yêu thích với độ sắc nét hoàn hảo.",
-    saleAndApplicationTime: {
-      from: "2024-04-09T00:00:00.000Z",
-      to: "2024-04-17T00:00:00.000Z",
-    },
-    depositTime: {
-      from: "2024-04-16T00:00:00.000Z",
-      to: "2024-04-19T00:00:00.000Z",
-    },
-    propertyInfo: {
-      startingPrice: 500000000,
-      applicationFee: 200000,
-      bidIncrement: 20000000,
-      deposit: 25000000,
-    },
-    auctionDetails: {
-      type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
-      method: "Phương thức trả giá lên",
-    },
-    auctionStartTime: "2024-04-20T09:00:00.000Z",
-    cover: [
-      require("./images/discount/discount-2.png"),
-      require("./images/discount/discount-2.png"),
-    ],
+    price: "$450",
   },
   {
-    id: "TSTPNT2024040905",
+    cover: "./images/discount/discount-3.png",
     name: "Sony PS4",
-    price: 50,
-    description:
-      "Mang đến cho bạn trải nghiệm hình ảnh sắc nét và sống động với Sony TV 1080p. Chiếc TV này được trang bị màn hình 1080p Full HD, mang đến hình ảnh chi tiết và rõ ràng hơn so với TV độ phân giải tiêu chuẩn. Công nghệ tiên tiến của Sony giúp nâng cao chất lượng hình ảnh, cho phép bạn thưởng thức các chương trình truyền hình, phim ảnh và trò chơi yêu thích với độ sắc nét hoàn hảo.",
-    saleAndApplicationTime: {
-      from: "2024-04-09T00:00:00.000Z",
-      to: "2024-04-17T00:00:00.000Z",
-    },
-    depositTime: {
-      from: "2024-04-16T00:00:00.000Z",
-      to: "2024-04-19T00:00:00.000Z",
-    },
-    propertyInfo: {
-      startingPrice: 500000000,
-      applicationFee: 200000,
-      bidIncrement: 20000000,
-      deposit: 25000000,
-    },
-    auctionDetails: {
-      type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
-      method: "Phương thức trả giá lên",
-    },
-    auctionStartTime: "2024-04-20T09:00:00.000Z",
-    cover: [
-      require("./images/discount/discount-3.png"),
-      require("./images/discount/discount-3.png"),
-    ],
+    price: "$50",
   },
   {
-    id: "TSTPNT2024040905",
+    cover: "./images/discount/discount-4.png",
     name: "Setgearr 2022",
-    price: 100,
-    description:
-      "Mang đến cho bạn trải nghiệm hình ảnh sắc nét và sống động với Sony TV 1080p. Chiếc TV này được trang bị màn hình 1080p Full HD, mang đến hình ảnh chi tiết và rõ ràng hơn so với TV độ phân giải tiêu chuẩn. Công nghệ tiên tiến của Sony giúp nâng cao chất lượng hình ảnh, cho phép bạn thưởng thức các chương trình truyền hình, phim ảnh và trò chơi yêu thích với độ sắc nét hoàn hảo.",
-    saleAndApplicationTime: {
-      from: "2024-04-09T00:00:00.000Z",
-      to: "2024-04-17T00:00:00.000Z",
-    },
-    depositTime: {
-      from: "2024-04-16T00:00:00.000Z",
-      to: "2024-04-19T00:00:00.000Z",
-    },
-    propertyInfo: {
-      startingPrice: 500000000,
-      applicationFee: 200000,
-      bidIncrement: 20000000,
-      deposit: 25000000,
-    },
-    auctionDetails: {
-      type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
-      method: "Phương thức trả giá lên",
-    },
-    auctionStartTime: "2024-04-20T09:00:00.000Z",
-    cover: [
-      require("./images/discount/discount-4.png"),
-      require("./images/discount/discount-4.png"),
-    ],
+    price: "$100",
   },
   {
-    id: "TSTPNT2024040905",
+    cover: "./images/discount/discount-5.png",
     name: "Tony BGB",
-    price: 20,
-    description:
-      "Mang đến cho bạn trải nghiệm hình ảnh sắc nét và sống động với Sony TV 1080p. Chiếc TV này được trang bị màn hình 1080p Full HD, mang đến hình ảnh chi tiết và rõ ràng hơn so với TV độ phân giải tiêu chuẩn. Công nghệ tiên tiến của Sony giúp nâng cao chất lượng hình ảnh, cho phép bạn thưởng thức các chương trình truyền hình, phim ảnh và trò chơi yêu thích với độ sắc nét hoàn hảo.",
-    saleAndApplicationTime: {
-      from: "2024-04-09T00:00:00.000Z",
-      to: "2024-04-17T00:00:00.000Z",
-    },
-    depositTime: {
-      from: "2024-04-16T00:00:00.000Z",
-      to: "2024-04-19T00:00:00.000Z",
-    },
-    propertyInfo: {
-      startingPrice: 500000000,
-      applicationFee: 200000,
-      bidIncrement: 20000000,
-      deposit: 25000000,
-    },
-    auctionDetails: {
-      type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
-      method: "Phương thức trả giá lên",
-    },
-    auctionStartTime: "2024-04-20T09:00:00.000Z",
-    cover: [
-      require("./images/discount/discount-5.png"),
-      require("./images/discount/discount-5.png"),
-    ],
+    price: "$20",
   },
   {
-    id: "TSTPNT2024040905",
+    cover: "./images/discount/discount-6.png",
     name: "RG products",
-    price: 200,
-    description:
-      "Mang đến cho bạn trải nghiệm hình ảnh sắc nét và sống động với Sony TV 1080p. Chiếc TV này được trang bị màn hình 1080p Full HD, mang đến hình ảnh chi tiết và rõ ràng hơn so với TV độ phân giải tiêu chuẩn. Công nghệ tiên tiến của Sony giúp nâng cao chất lượng hình ảnh, cho phép bạn thưởng thức các chương trình truyền hình, phim ảnh và trò chơi yêu thích với độ sắc nét hoàn hảo.",
-    saleAndApplicationTime: {
-      from: "2024-04-09T00:00:00.000Z",
-      to: "2024-04-17T00:00:00.000Z",
-    },
-    depositTime: {
-      from: "2024-04-16T00:00:00.000Z",
-      to: "2024-04-19T00:00:00.000Z",
-    },
-    propertyInfo: {
-      startingPrice: 500000000,
-      applicationFee: 200000,
-      bidIncrement: 20000000,
-      deposit: 25000000,
-    },
-    auctionDetails: {
-      type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
-      method: "Phương thức trả giá lên",
-    },
-    auctionStartTime: "2024-04-20T09:00:00.000Z",
-    cover: [
-      require("./images/discount/discount-6.png"),
-      require("./images/discount/discount-6.png"),
-    ],
+    price: "$200",
   },
   {
-    id: "TSTPNT2024040905",
+    cover: "./images/discount/discount-7.png",
     name: "Ranasonic 2022",
-    price: 300,
-    description:
-      "Mang đến cho bạn trải nghiệm hình ảnh sắc nét và sống động với Sony TV 1080p. Chiếc TV này được trang bị màn hình 1080p Full HD, mang đến hình ảnh chi tiết và rõ ràng hơn so với TV độ phân giải tiêu chuẩn. Công nghệ tiên tiến của Sony giúp nâng cao chất lượng hình ảnh, cho phép bạn thưởng thức các chương trình truyền hình, phim ảnh và trò chơi yêu thích với độ sắc nét hoàn hảo.",
-    saleAndApplicationTime: {
-      from: "2024-04-09T00:00:00.000Z",
-      to: "2024-04-17T00:00:00.000Z",
-    },
-    depositTime: {
-      from: "2024-04-16T00:00:00.000Z",
-      to: "2024-04-19T00:00:00.000Z",
-    },
-    propertyInfo: {
-      startingPrice: 500000000,
-      applicationFee: 200000,
-      bidIncrement: 20000000,
-      deposit: 25000000,
-    },
-    auctionDetails: {
-      type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
-      method: "Phương thức trả giá lên",
-    },
-    auctionStartTime: "2024-04-20T09:00:00.000Z",
-    cover: [
-      require("./images/discount/discount-7.png"),
-      require("./images/discount/discount-7.png"),
-    ],
+    price: "$300",
   },
   {
-    id: "TSTPNT2024040905",
+    cover: "./images/discount/discount-8.png",
     name: "Pune HD",
-    price: 30,
-    description:
-      "Mang đến cho bạn trải nghiệm hình ảnh sắc nét và sống động với Sony TV 1080p. Chiếc TV này được trang bị màn hình 1080p Full HD, mang đến hình ảnh chi tiết và rõ ràng hơn so với TV độ phân giải tiêu chuẩn. Công nghệ tiên tiến của Sony giúp nâng cao chất lượng hình ảnh, cho phép bạn thưởng thức các chương trình truyền hình, phim ảnh và trò chơi yêu thích với độ sắc nét hoàn hảo.",
-    saleAndApplicationTime: {
-      from: "2024-04-09T00:00:00.000Z",
-      to: "2024-04-17T00:00:00.000Z",
-    },
-    depositTime: {
-      from: "2024-04-16T00:00:00.000Z",
-      to: "2024-04-19T00:00:00.000Z",
-    },
-    propertyInfo: {
-      startingPrice: 500000000,
-      applicationFee: 200000,
-      bidIncrement: 20000000,
-      deposit: 25000000,
-    },
-    auctionDetails: {
-      type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
-      method: "Phương thức trả giá lên",
-    },
-    auctionStartTime: "2024-04-20T09:00:00.000Z",
-    cover: [
-      require("./images/discount/discount-8.png"),
-      require("./images/discount/discount-8.png"),
-    ],
+    price: "$30",
   },
   {
-    id: "TSTPNT2024040905",
+    cover: "./images/discount/discount-9.png",
     name: "Sony CCTV",
-    price: 80,
-    description:
-      "Mang đến cho bạn trải nghiệm hình ảnh sắc nét và sống động với Sony TV 1080p. Chiếc TV này được trang bị màn hình 1080p Full HD, mang đến hình ảnh chi tiết và rõ ràng hơn so với TV độ phân giải tiêu chuẩn. Công nghệ tiên tiến của Sony giúp nâng cao chất lượng hình ảnh, cho phép bạn thưởng thức các chương trình truyền hình, phim ảnh và trò chơi yêu thích với độ sắc nét hoàn hảo.",
-    saleAndApplicationTime: {
-      from: "2024-04-09T00:00:00.000Z",
-      to: "2024-04-17T00:00:00.000Z",
-    },
-    depositTime: {
-      from: "2024-04-16T00:00:00.000Z",
-      to: "2024-04-19T00:00:00.000Z",
-    },
-    propertyInfo: {
-      startingPrice: 500000000,
-      applicationFee: 200000,
-      bidIncrement: 20000000,
-      deposit: 25000000,
-    },
-    auctionDetails: {
-      type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
-      method: "Phương thức trả giá lên",
-    },
-    auctionStartTime: "2024-04-20T09:00:00.000Z",
-    cover: [
-      require("./images/discount/discount-9.png"),
-      require("./images/discount/discount-9.png"),
-    ],
+    price: "$80",
   },
 ];
 export default Ddata;

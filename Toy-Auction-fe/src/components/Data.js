c215cbb8891fbebf9c988c7df1eeb606dca4974e
NGUYEN QUY <nngocquy75@gmail.com>
2024-04-27 13:51:08 +0700

    first commit

--- Toy-Auction-fe/src/components/Data.js
+++ Toy-Auction-fe/src/components/Data.js
@@ -1,47 +1,228 @@
 const Data = {
   productItems: [
     {
+      //desperate
+      discount: 50,
+      price: 100,
+
       id: 1,
-      discount: 50,
-      cover: "./images/flash/flash-1.png",
-      name: "Shoes",
-      price: 100,
+      cover: [
+        require("./images/flash/flash-1.png"),
+        require("./images/flash/flash-2.png"),
+      ],
+      name: "Lamborghini urus ",
+      description: "Mô tả sản phẩm giày.",
+      saleAndApplicationTime: {
+        from: "2024-04-09T00:00:00.000Z",
+        to: "2024-04-17T00:00:00.000Z",
+      },
+      depositTime: {
+        from: "2024-04-16T00:00:00.000Z",
+        to: "2024-04-19T00:00:00.000Z",
+      },
+      propertyInfo: {
+        startingPrice: 500000000,
+        applicationFee: 200000,
+        bidIncrement: 20000000,
+        deposit: 25000000,
+      },
+      auctionDetails: {
+        type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
+        method: "Phương thức trả giá lên",
+      },
+      description: [
+        "- Tranh Gà Trống, bột màu, kích thước 30 x 38 cm, ký đề nge, 81.",
+        "- Tranh vẽ Gà Trống, là biểu tượng rõ nét của nghệ thuật phương Đông (yếu tố Ngũ hành) gắn kết với tâm sinh lý con người. Bên cạnh họa sĩ Nguyễn Tư Nghiêm tiên phong thì Hội mỹ thuật Việt Nam cũng cổ súy rất rõ nét, qua loạt nghiên cứu của học giả Lê Văn Sửu trên Tạp chí Mỹ thuật Việt Nam, suốt cả năm 1987.",
+        "- Theo sách cổ, người sinh năm Gà, Tân Dậu, 1981, Gà biểu tượng bởi ngũ hành Kim, ở phương Tây, là chiếc Chuông chùa của Phật giáo. Tân thuộc thiên can Kim, Dậu thuộc địa chi Kim, người sinh năm 1981 sẽ có đặc điểm giống chú gà trống với đôi chân to, cùng những cái cựa sắc, nhọn, biểu hiện của hành Kim, người sinh năm này có tố chất khỏe mạnh, xương cốt cứng cáp, ý chí sắc bén.",
+        "- Họa sĩ vẽ Gà Trống, thuộc tính gáy vang vọng như tiếng chuông chùa, báo hiệu một thế hệ sinh ra có sức thu hút, có danh tiếng, có lòng nhân hậu. Sắc đỏ, vàng rực của mào, của đuôi, của đôi chân gà, đúng thuộc tính ngũ hành 1981, mệnh mộc sinh hỏa, sẽ hàm ý người năm này có nhiều năng lượng tốt lành, tươi vui.",
+      ],
     },
     {
-      id: 2,
-      discount: 40,
-      cover: "./images/flash/flash-2.png",
-      name: "Watch",
+      //desperate
+      discount: 50,
       price: 20,
+
+      id: 2,
+      cover: [
+        require("./images/flash/flash-2.png"),
+        require("./images/flash/flash-2.png"),
+      ],
+      name: "Watch",
+      description: "Mô tả sản phẩm đồng hồ.",
+      saleAndApplicationTime: {
+        from: "2024-04-09T00:00:00.000Z",
+        to: "2024-04-17T00:00:00.000Z",
+      },
+      depositTime: {
+        from: "2024-04-16T00:00:00.000Z",
+        to: "2024-04-19T00:00:00.000Z",
+      },
+      propertyInfo: {
+        startingPrice: 500000000,
+        applicationFee: 200000,
+        bidIncrement: 20000000,
+        deposit: 25000000,
+      },
+      auctionDetails: {
+        type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
+        method: "Phương thức trả giá lên",
+      },
     },
     {
-      id: 3,
-      discount: 40,
-      cover: "./images/flash/flash-3.png",
-      name: "Smart Mobile Black",
+      //desperate
+      discount: 50,
       price: 200,
+
+      id: 3,
+      cover: [
+        require("./images/flash/flash-3.png"),
+        require("./images/flash/flash-3.png"),
+      ],
+      name: "Smart Mobile Black",
+      description: "Mô tả sản phẩm điện thoại thông minh màu đen.",
+      saleAndApplicationTime: {
+        from: "2024-04-09T00:00:00.000Z",
+        to: "2024-04-17T00:00:00.000Z",
+      },
+      depositTime: {
+        from: "2024-04-16T00:00:00.000Z",
+        to: "2024-04-19T00:00:00.000Z",
+      },
+      propertyInfo: {
+        startingPrice: 500000000,
+        applicationFee: 200000,
+        bidIncrement: 20000000,
+        deposit: 25000000,
+      },
+      auctionDetails: {
+        type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
+        method: "Phương thức trả giá lên",
+      },
     },
     {
-      id: 4,
-      discount: 40,
-      cover: "./images/flash/flash-4.png",
-      name: "Smart Watch Black",
+      //desperate
+      discount: 50,
       price: 50,
+
+      id: 4,
+      cover: [
+        require("./images/flash/flash-4.png"),
+        require("./images/flash/flash-4.png"),
+      ],
+      name: "Smart Watch Black",
+      description: "Mô tả sản phẩm đồng hồ thông minh màu đen.",
+      saleAndApplicationTime: {
+        from: "2024-04-09T00:00:00.000Z",
+        to: "2024-04-17T00:00:00.000Z",
+      },
+      depositTime: {
+        from: "2024-04-16T00:00:00.000Z",
+        to: "2024-04-19T00:00:00.000Z",
+      },
+      propertyInfo: {
+        startingPrice: 500000000,
+        applicationFee: 200000,
+        bidIncrement: 20000000,
+        deposit: 25000000,
+      },
+      auctionDetails: {
+        type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
+        method: "Phương thức trả giá lên",
+      },
     },
     {
+      //desperate
+      discount: 50,
+      price: 100,
+
       id: 5,
-      discount: 50,
-      cover: "./images/flash/flash-1.png",
+      cover: [
+        require("./images/flash/flash-1.png"),
+        require("./images/flash/flash-1.png"),
+      ],
       name: "Shoes",
-      price: 100,
+      description: "Mô tả sản phẩm giày.",
+      saleAndApplicationTime: {
+        from: "2024-04-09T00:00:00.000Z",
+        to: "2024-04-17T00:00:00.000Z",
+      },
+      depositTime: {
+        from: "2024-04-16T00:00:00.000Z",
+        to: "2024-04-19T00:00:00.000Z",
+      },
+      propertyInfo: {
+        startingPrice: 500000000,
+        applicationFee: 200000,
+        bidIncrement: 20000000,
+        deposit: 25000000,
+      },
+      auctionDetails: {
+        type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
+        method: "Phương thức trả giá lên",
+      },
     },
     {
-      id: 6,
+      //desperate
       discount: 50,
-      cover: "./images/flash/flash-3.png",
-      name: "Shoes",
       price: 100,
+
+      id: 6,
+      cover: [
+        require("./images/flash/flash-3.png"),
+        require("./images/flash/flash-3.png"),
+      ],
+      name: "Shoes",
+      description: "Mô tả sản phẩm giày.",
+      saleAndApplicationTime: {
+        from: "2024-04-09T00:00:00.000Z",
+        to: "2024-04-17T00:00:00.000Z",
+      },
+      depositTime: {
+        from: "2024-04-16T00:00:00.000Z",
+        to: "2024-04-19T00:00:00.000Z",
+      },
+      propertyInfo: {
+        startingPrice: 500000000,
+        applicationFee: 200000,
+        bidIncrement: 20000000,
+        deposit: 25000000,
+      },
+      auctionDetails: {
+        type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
+        method: "Phương thức trả giá lên",
+      },
+    },
+    {
+      //desperate
+      discount: 50,
+      price: 100,
+
+      id: 7,
+      cover: [
+        require("./images/flash/flash-3.png"),
+        require("./images/flash/flash-3.png"),
+      ],
+      name: "Lamboghini",
+      description: "Mô tả sản phẩm giày.",
+      saleAndApplicationTime: {
+        from: "2024-04-09T00:00:00.000Z",
+        to: "2024-04-17T00:00:00.000Z",
+      },
+      depositTime: {
+        from: "2024-04-16T00:00:00.000Z",
+        to: "2024-04-19T00:00:00.000Z",
+      },
+      propertyInfo: {
+        startingPrice: 500000000,
+        applicationFee: 200000,
+        bidIncrement: 20000000,
+        deposit: 25000000,
+      },
+      auctionDetails: {
+        type: "Đấu giá trực tiếp bằng lời nói tại cuộc đấu giá",
+        method: "Phương thức trả giá lên",
+      },
     },
   ],
-}
-export default Data
+};
+export default Data;

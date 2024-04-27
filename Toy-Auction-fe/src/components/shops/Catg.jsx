939b106b1d2e631a1c244dab4547e7adb7e784ad
Trần Văn Tài <taiho58@gmail.com>
2024-04-27 18:30:37 +0700

    style(Catg): change font color

--- Toy-Auction-fe/src/components/shops/Catg.jsx
+++ Toy-Auction-fe/src/components/shops/Catg.jsx
@@ -40,18 +40,21 @@ const Catg = () => {
     <>
       <div className="category">
         <div className="chead d_flex">
-          <h1>Brands </h1>
-          <h1>Shops </h1>
+          <h1 className="text-base font-bold">Brands </h1>
+          <h1 className="text-base font-extrabold text-primary-500">Shops </h1>
         </div>
         {branches.map((branch, index) => {
           return (
-            <div className="box f_flex" key={index}>
-              <img src={branch.img} alt="" />
-              <span>{branch.name}</span>
+            <div
+              className="box f_flex hover:bg-primary text-primary-600 hover:text-white items-center"
+              key={index}
+            >
+              <img src={branch.img} alt="" className="w-5 h-5" />
+              <span className="text-sm font-bold">{branch.name}</span>
             </div>
           );
         })}
-        <div className="box box2">
+        <div className="box box2 hover:bg-primary text-primary-600 hover:text-white">
           <button>View All Brands</button>
         </div>
       </div>

0d3a191662e4b5fc88b5380a09f9cfe048a6f453
Trần Văn Tài <taiho58@gmail.com>
2024-04-27 09:38:23 +0700

    feat(model): add auction model and refactor product model

---
+++ Toy-Auction-be/app/models/auction.model.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+
+const BidSchema = mongoose.Schema({
+  bidder: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  amount: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const AuctionSchema = mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+    saleAndApplicationTime: {
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+        required: true,
+      },
+    },
+    depositTime: {
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+        required: true,
+      },
+    },
+    propertyInfo: {
+      startingPrice: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+      applicationFee: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+      bidIncrement: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+      deposit: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+    },
+    auctionDetails: {
+      type: {
+        type: String,
+        required: true,
+      },
+      method: {
+        type: String,
+        required: true,
+      },
+    },
+    bids: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Bid",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Auction = mongoose.model("Auction", AuctionSchema);
+const Bid = mongoose.model("Bid", BidSchema);
+
+module.exports = { Auction, Bid };
--- Toy-Auction-be/app/models/product.model.js
+++ Toy-Auction-be/app/models/product.model.js
@@ -21,66 +21,6 @@ const ProductSchema = mongoose.Schema(
       required: true,
       trim: true,
     },
-
-    // Khung thời gian bán và đăng ký tham gia đấu giá
-    saleAndApplicationTime: {
-      from: {
-        type: Date,
-        required: true,
-      },
-      to: {
-        type: Date,
-        required: true,
-      },
-    },
-
-    // Khung thời gian đặt cọc
-    depositTime: {
-      from: {
-        type: Date,
-        required: true,
-      },
-      to: {
-        type: Date,
-        required: true,
-      },
-    },
-
-    // Thông tin chi tiết về tài sản
-    propertyInfo: {
-      startingPrice: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      applicationFee: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      bidIncrement: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      deposit: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-    },
-
-    // Thông tin chi tiết về phiên đấu giá
-    auctionDetails: {
-      type: {
-        type: String,
-        required: true,
-      },
-      method: {
-        type: String,
-        required: true,
-      },
-    },
   },
   {
     timestamps: true,
